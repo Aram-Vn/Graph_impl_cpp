27a4e382ce9988b2d75d703b89f957e5bcfa7474
Aram-Vn <aramvardanyan13@gmail.com>
2024-01-15 17:02:41 +0400

    add comments to WeightedGraph

--- weighted_graph/src/weighted_graph.h
+++ weighted_graph/src/weighted_graph.h
@@ -9,17 +9,20 @@
 class WeightedGraph
 {
 public:
-    WeightedGraph(int size);
+    WeightedGraph(int size); // Constructor to initialize the weighted graph with a given number of vertices
 
 public:
-    void add_edge(int vertex, int vertex1, int weight);
-    std::vector<int> dijkstra(int curVert);
+    void add_edge(int vertex, int vertex1, int weight); // Add a weighted edge between two vertices in the graph
+    std::vector<int> dijkstra(int curVert); /* Perform Dijkstra's algorithm to find the shortest paths from a
+                                             * source vertex to all other vertecces */
 
 private:
-    void dijkstra_helper(int vertex, std::vector<int>& fast);
+    void dijkstra_helper(int vertex, std::vector<int>& fast); // Helper function for Dijkstra's algorithm
 
 private:
-    std::vector<std::vector<std::pair<int, int>>> vec;
+    std::vector<std::vector<std::pair<int, int>>> vec; /* Adjacency list representation of the weighted graph
+                                                        * Each element is a pair representing a connected vertex and the
+                                                        * weight of the edge */
 };
 
 #endif // WEIGHTED_GRAPH_H
